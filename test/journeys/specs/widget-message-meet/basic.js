0c4915b1129476f8b95f53f39fb7a76a46fee7a1
Adam Weeks <adweeks@cisco.com>
2017-04-05 15:31:49 -0400

    test(journey): add menu functionality tests

--- test/journeys/specs/widget-message-meet/basic.js
+++ test/journeys/specs/widget-message-meet/basic.js
@@ -44,4 +44,57 @@ describe(`Widget Message Meet`, () => {
     browser.waitUntil(() => $(`h1`).getText() !== mccoy.email);
     assert.equal($(`h1`).getText(), mccoy.displayName);
   });
+
+  describe(`menu functionality`, () => {
+    const menuButton = `.ciscospark-activity-menu-button-wrapper button`;
+    const exitButton = `.ciscospark-activity-menu-exit button`;
+    const messageButton = `button[aria-label="Message"]`;
+    const meetButton = `button[aria-label="Call"]`;
+    const activityMenu = `.ciscospark-activity-menu`;
+    const controlsContainer = `.ciscospark-controls-container`;
+    const messageWidget = `.ciscospark-message-component-wrapper`;
+    const meetWidget = `.ciscospark-meet-component-wrapper`;
+    it(`has a menu button`, () => {
+      assert.isTrue(browser.isVisible(menuButton));
+    });
+
+    it(`displays the menu when clicking the menu button`, () => {
+      browser.click(menuButton);
+      browser.waitForVisible(activityMenu);
+    });
+
+    it(`has an exit menu button`, () => {
+      assert.isTrue(browser.isVisible(activityMenu));
+      browser.waitForVisible(exitButton);
+    });
+
+    it(`closes the menu with the exit button`, () => {
+      browser.click(exitButton);
+      browser.waitForVisible(activityMenu, 1500, true);
+    });
+
+    it(`has a message button`, () => {
+      browser.click(menuButton);
+      browser.element(controlsContainer).element(messageButton).waitForVisible();
+    });
+
+    it(`switches to message widget`, () => {
+      browser.element(controlsContainer).element(messageButton).click();
+      assert.isTrue(browser.isVisible(messageWidget));
+      assert.isFalse(browser.isVisible(meetWidget));
+    });
+
+    it(`has a meet button`, () => {
+      browser.click(menuButton);
+      browser.element(controlsContainer).element(meetButton).waitForVisible();
+    });
+
+    it(`switches to meet widget`, () => {
+      browser.element(controlsContainer).element(meetButton).click();
+      assert.isTrue(browser.isVisible(meetWidget));
+      assert.isFalse(browser.isVisible(messageWidget));
+    });
+
+  });
+
 });
