04f91ec85dd6a948ef003b44889930b3362e2a3f
Bernie Zang <bernie.zang@gmail.com>
2017-06-28 16:42:40 -0400

    chore(tooling): Update cjs and es builds to generate css modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
-module.exports = {
+/* eslint-disable no-empty-function */
+module.exports = (ctx) => ({
   plugins: {
+    'postcss-modules': {
+      getJSON: ctx.extractModules || (() => {})
+    },
     'postcss-cssnext': {
       browsers: [`last 2 versions`, `IE > 10`]
     },
+    cssnano: {
+      preset: `default`,
+      autoprefixer: false
+    },
     'postcss-reporter': {}
   }
-};
+});
