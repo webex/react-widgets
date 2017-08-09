1fdf80b5065bef45aa4812c5189dddcc08d5ffde
Adam Weeks <adweeks@cisco.com>
2017-08-09 11:54:17 -0400

    chore: add spark upgrade script

---
+++ scripts/utils/update.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env babel-node
+/**
+ * This script allows you to upgrade all of the @ciscospark dependencies
+ * to their latest version.
+ */
+
+const ncu = require(`npm-check-updates`);
+
+ncu.run({
+  // Always specify the path to the package file
+  packageFile: `package.json`,
+  // Any command-line option can be specified here.
+  // These are set by default:
+  filter: /^@ciscospark.*$/,
+  upgrade: true
+}).then(() => {
+  console.log(`@ciscospark dependencies upgraded`);
+});
