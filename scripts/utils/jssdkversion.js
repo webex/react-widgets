b2ffafc5ec1b2502ecbea7da86f9537fc614d846
Adam Weeks <adweeks@cisco.com>
2017-08-10 12:26:43 -0400

    chore: add jssdk jenkins script

---
+++ scripts/utils/jssdkversion.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env babel-node
+/**
+ * Gets the highest version of @ciscospark js-sdk packages
+ * for use in the Jenkinsfile.jssdk script
+ */
+const semver = require(`semver`);
+const psjson = require(`../../package.json`);
+
+const dependencies = Object.keys(psjson.dependencies)
+  .filter((key) => key.includes(`@ciscospark`) && !key.includes(`@ciscospark/eslint-config`))
+  .map((key) => psjson.dependencies[key].replace(`^`, ``).replace(`~`, ``))
+  // eslint-disable-next-line arrow-body-style
+  .reduce((maxVer, currentVer) => {
+    return semver.gt(currentVer, maxVer) ? currentVer : maxVer;
+  });
+
+const devDependencies = Object.keys(psjson.devDependencies)
+  .filter((key) => key.includes(`@ciscospark`) && !key.includes(`@ciscospark/eslint-config`))
+  .map((key) => psjson.devDependencies[key].replace(`^`, ``).replace(`~`, ``))
+  // eslint-disable-next-line arrow-body-style
+  .reduce((maxVer, currentVer) => {
+    return semver.gt(currentVer, maxVer) ? currentVer : maxVer;
+  });
+
+const max = semver.gt(dependencies, devDependencies) ? dependencies : devDependencies;
+console.log(max);
