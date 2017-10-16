8d52d5cff12066666de353e032f8d383c3ef2d9d
Bernie Zang <nzang@cisco.com>
2017-10-16 15:27:18 -0400

    refactor(widget-message-meet): Remove widget

--- src/index.js
+++ src/index.js
@@ -26,5 +26,5 @@ export {default as TextArea} from '@ciscospark/react-component-textarea';
 export {default as TitleBar} from '@ciscospark/react-component-title-bar';
 export {default as TypingAvatar} from '@ciscospark/react-component-typing-avatar';
 export {default as TypingIndicator} from '@ciscospark/react-component-typing-indicator';
-export {default as WidgetMessageMeet} from '@ciscospark/widget-message-meet';
 export {default as WidgetSpace} from '@ciscospark/widget-space';
+export {default as WidgetRecents} from '@ciscospark/widget-recents';
