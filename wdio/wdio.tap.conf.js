b40288d3ea7adc3dafece9bc5f6e16998ddab8b3
Bernie Zang <nzang@cisco.com>
2018-03-06 16:51:08 -0500

    fix(tooling): Add sauce to wdio configs

--- wdio/wdio.tap.conf.js
+++ wdio/wdio.tap.conf.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const config = require('./wdio.sauce.conf.js');
+const {config} = require('./wdio.sauce.conf.js');
 
 config.baseUrl = 'https://code.s4d.io';
 config.sauceConnect = false;
