8043635fbdc903b06eba272bffafe762f0f1fc22
Bernie Zang <nzang@cisco.com>
2018-03-15 11:14:07 -0400

    chore: update husky, add precommit scripts

--- scripts/validate-ci-config.sh
+++ scripts/validate-ci-config.sh
@@ -3,12 +3,16 @@
 # The following line is needed by the CircleCI Local Build Tool (due to Docker interactivity)
 exec < /dev/tty
 
-# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
-if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
-	echo "CircleCI Configuration Failed Validation."
-	echo $eMSG
-	exit 1
+if hash circleci 2>/dev/null; then
+  # If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+  if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
+    echo "CircleCI Configuration Failed Validation."
+    echo $eMSG
+    exit 1
+  else
+    echo "CircleCI Configuration Validated."
+    exit 0
+  fi
 fi
 
-echo "CircleCI Configuration Validated."
 exit 0
