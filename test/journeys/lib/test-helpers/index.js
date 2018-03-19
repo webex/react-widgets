b551f320696330371c4f9908885cda810ce66af5
Bernie Zang <bernie.zang@gmail.com>
2018-03-19 11:29:54 -0400

    chore(test): Add test user setup helper

--- test/journeys/lib/test-helpers/index.js
+++ test/journeys/lib/test-helpers/index.js
@@ -1,5 +1,12 @@
 import uuid from 'uuid';
 
+import '@ciscospark/internal-plugin-conversation';
+import '@ciscospark/plugin-logger';
+import '@ciscospark/plugin-phone';
+
+import testUsers from '@ciscospark/test-helper-test-users';
+import CiscoSpark from '@ciscospark/spark-core';
+
 /**
  * Move mouse a specified amount of pixels
  * Origin is set to the element that matches the selector passed
@@ -40,4 +47,108 @@ export function moveMouse(aBrowser, selector) {
   else {
     aBrowser.moveToObject(selector);
   }
+}
+
+/**
+ * Creates our Back to the Future Test Users for group meeting tests
+ * @returns {Object}
+ */
+export function setupGroupTestUsers() {
+  let docbrown, marty, lorraine;
+  testUsers.create({count: 1, config: {displayName: 'Marty McFly'}})
+    .then((users) => {
+      [marty] = users;
+      marty.spark = new CiscoSpark({
+        credentials: {
+          authorization: marty.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+    });
+
+  testUsers.create({count: 1, config: {displayName: 'Emmett Brown'}})
+    .then((users) => {
+      [docbrown] = users;
+      docbrown.spark = new CiscoSpark({
+        credentials: {
+          authorization: docbrown.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+    });
+
+  testUsers.create({count: 1, config: {displayName: 'Lorraine Baines'}})
+    .then((users) => {
+      [lorraine] = users;
+      lorraine.spark = new CiscoSpark({
+        credentials: {
+          authorization: lorraine.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+    });
+
+  browser.waitUntil(() =>
+    marty && marty.email && marty.spark && marty.spark.canAuthorize &&
+    docbrown && docbrown.email && docbrown.spark && docbrown.spark.canAuthorize &&
+    lorraine && lorraine.email && lorraine.spark && lorraine.spark.canAuthorize,
+  10000, 'failed to create test users');
+
+  return {marty, docbrown, lorraine};
+}
+
+/**
+ * Creates our Star Trek Test Users for one on one meeting tests
+ * @returns {Object}
+ */
+export function setupOneOnOneUsers() {
+  let mccoy, spock;
+  testUsers.create({count: 1, config: {displayName: 'Mr Spock'}})
+    .then((users) => {
+      [spock] = users;
+      spock.spark = new CiscoSpark({
+        credentials: {
+          authorization: spock.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+    });
+
+  testUsers.create({count: 1, config: {displayName: 'Bones Mccoy'}})
+    .then((users) => {
+      [mccoy] = users;
+      mccoy.spark = new CiscoSpark({
+        credentials: {
+          authorization: mccoy.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+    });
+
+  browser.waitUntil(() =>
+    mccoy && mccoy.email && mccoy.spark && mccoy.spark.canAuthorize &&
+    spock && spock.email && spock.spark && spock.spark.canAuthorize,
+  15000, 'failed to create test users');
+
+  return {mccoy, spock};
 }
--- test/journeys/lib/test-helpers/space-widget/main.js
+++ test/journeys/lib/test-helpers/space-widget/main.js
@@ -1,10 +1,3 @@
-import '@ciscospark/internal-plugin-conversation';
-import '@ciscospark/plugin-logger';
-import '@ciscospark/plugin-phone';
-
-import testUsers from '@ciscospark/test-helper-test-users';
-import CiscoSpark from '@ciscospark/spark-core';
-
 export const elements = {
   spaceWidget: '.ciscospark-space-widget',
   menuButton: 'button[aria-label="Main Menu"]',
@@ -22,17 +15,30 @@ export const elements = {
 };
 
 /**
+ * Opens activity menu and clicks a button inside it
+ * @param {Browser} aBrowser
+ * @param {string} buttonToClick element selector
+ */
+export function openMenuAndClickButton(aBrowser, buttonToClick) {
+  if (!aBrowser.isVisible(elements.activityMenu)) {
+    aBrowser.waitUntil(() =>
+      aBrowser.isVisible(elements.menuButton),
+    5000, 'menu button is not visible when trying to open activity menu');
+    aBrowser.click(elements.menuButton);
+    aBrowser.waitUntil(() =>
+      aBrowser.isVisible(elements.activityMenu),
+    5000, 'could not open activity menu');
+  }
+  aBrowser.click(`${elements.activityMenu} ${buttonToClick}`);
+}
+
+/**
  * Switches to message widget
  * @param {Object} aBrowser
  * @returns {void}
  */
 export function switchToMessage(aBrowser) {
-  if (!aBrowser.isVisible(elements.activityMenu)) {
-    aBrowser.click(elements.menuButton);
-    aBrowser.waitForVisible(elements.activityMenu);
-  }
-  aBrowser.waitForVisible(`${elements.activityMenu} ${elements.messageButton}`);
-  aBrowser.click(elements.messageButton);
+  openMenuAndClickButton(aBrowser, elements.messageButton);
 }
 
 /**
@@ -41,106 +47,5 @@ export function switchToMessage(aBrowser) {
  * @returns {void}
  */
 export function switchToMeet(aBrowser) {
-  if (!aBrowser.isVisible(elements.activityMenu)) {
-    aBrowser.waitForVisible(elements.menuButton);
-    aBrowser.click(elements.menuButton);
-    aBrowser.waitForVisible(elements.activityMenu);
-  }
-  aBrowser.waitForVisible(`${elements.activityMenu} ${elements.meetButton}`);
-  aBrowser.click(elements.meetButton);
-}
-
-/**
- * Opens activity menu and clicks a button inside it
- * @param {Browser} aBrowser
- * @param {string} buttonToClick element selector
- */
-export function openMenuAndClickButton(aBrowser, buttonToClick) {
-  aBrowser.click(elements.menuButton);
-  aBrowser.waitForVisible(elements.activityMenu);
-  aBrowser.click(`${elements.activityMenu} ${buttonToClick}`);
-}
-
-
-/**
- * Creates our Back to the Future Test Users for group meeting tests
- * @returns {Object}
- */
-export function setupGroupTestUsers() {
-  let docbrown, marty, lorraine;
-  testUsers.create({count: 1, config: {displayName: 'Marty McFly'}})
-    .then((users) => {
-      [marty] = users;
-      marty.spark = new CiscoSpark({
-        credentials: {
-          authorization: marty.token
-        },
-        config: {
-          logger: {
-            level: 'error'
-          }
-        }
-      });
-    });
-
-  testUsers.create({count: 1, config: {displayName: 'Emmett Brown'}})
-    .then((users) => {
-      [docbrown] = users;
-      docbrown.spark = new CiscoSpark({
-        credentials: {
-          authorization: docbrown.token
-        },
-        config: {
-          logger: {
-            level: 'error'
-          }
-        }
-      });
-    });
-
-  testUsers.create({count: 1, config: {displayName: 'Lorraine Baines'}})
-    .then((users) => {
-      [lorraine] = users;
-      lorraine.spark = new CiscoSpark({
-        credentials: {
-          authorization: lorraine.token
-        },
-        config: {
-          logger: {
-            level: 'error'
-          }
-        }
-      });
-    });
-
-  browser.waitUntil(() =>
-    marty && marty.spark && marty.spark.canAuthorize &&
-    docbrown && docbrown.spark && docbrown.spark.canAuthorize &&
-    lorraine && lorraine.spark && lorraine.spark.canAuthorize,
-  10000, 'failed to create test users');
-
-  return {marty, docbrown, lorraine};
-}
-
-/**
- * Creates our Star Trek Test Users for one on one meeting tests
- * @returns {Object}
- */
-export function setupOneOnOneUsers() {
-  let mccoy, spock;
-  testUsers.create({count: 1, config: {displayName: 'Mr Spock'}})
-    .then((users) => {
-      [spock] = users;
-    });
-
-  testUsers.create({count: 1, config: {displayName: 'Bones Mccoy'}})
-    .then((users) => {
-      [mccoy] = users;
-    });
-
-  browser.waitUntil(() =>
-    mccoy && mccoy.email && spock && spock.email,
-  15000, 'failed to create test users');
-
-  return {mccoy, spock};
+  openMenuAndClickButton(aBrowser, elements.meetButton);
 }
