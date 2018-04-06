41e0ba9507f8862b494c45d47a2bb20f1529ff5a
moriahmaney <moriahmaney@gmail.com>
2018-04-06 14:23:36 -0400

    refactor(journeys): set answering side to start on message widget start call setting test

--- test/journeys/specs/oneOnOne/dataApi/startup-settings.js
+++ test/journeys/specs/oneOnOne/dataApi/startup-settings.js
@@ -75,11 +75,11 @@ describe('Widget Space: One on One: Data API Settings', () => {
         csmmDom.setAttribute('data-toggle', 'ciscospark-space');
         csmmDom.setAttribute('data-access-token', localAccessToken);
         csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'meet');
+        csmmDom.setAttribute('data-initial-activity', 'message');
         document.getElementById('ciscospark-widget').appendChild(csmmDom);
         window.loadBundle('/dist-space/bundle.js');
       }, mccoy.token.access_token, spock.email);
-      browserRemote.waitForVisible(elements.meetButton);
+      browserRemote.waitForVisible(elements.messageWidget);
     });
 
     before('inject token', () => {
