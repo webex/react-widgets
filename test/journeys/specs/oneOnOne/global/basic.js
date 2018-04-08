cdbb82a11007ab2aa3b6b8e785b73f9d02aabe9a
moriahmaney <moriahmaney@gmail.com>
2018-04-07 21:29:02 -0400

    refactor(journeys): use consistent style across global one on one tests

--- test/journeys/specs/oneOnOne/global/basic.js
+++ test/journeys/specs/oneOnOne/global/basic.js
@@ -9,187 +9,176 @@ import {runAxe} from '../../../lib/axe';
 import {elements, openMenuAndClickButton} from '../../../lib/test-helpers/space-widget/main';
 
 describe('Widget Space: One on One', () => {
-  describe('Global', () => {
-    const browserLocal = browser.select('browserLocal');
+  const browserLocal = browser.select('browserLocal');
 
-    let mccoy, spock;
-    const mccoyName = 'Bones Mccoy';
-    const spockName = 'Mr Spock';
+  let mccoy, spock;
+  const mccoyName = 'Bones Mccoy';
+  const spockName = 'Mr Spock';
 
-    before('load browsers', () => {
-      browserLocal
-        .url('/space.html?basic')
-        .execute(() => {
-          localStorage.clear();
-        });
-    });
+  before('load browsers', () => {
+    browserLocal.url('/space.html?basic');
+  });
 
-    before('create spock', () => testUsers.create({count: 1, config: {displayName: spockName}})
-      .then((users) => {
-        [spock] = users;
-        spock.spark = new CiscoSpark({
-          credentials: {
-            authorization: spock.token
-          },
-          config: {
-            logger: {
-              level: 'error'
-            }
+  before('create spock', () => testUsers.create({count: 1, config: {displayName: spockName}})
+    .then((users) => {
+      [spock] = users;
+      spock.spark = new CiscoSpark({
+        credentials: {
+          authorization: spock.token
+        },
+        config: {
+          logger: {
+            level: 'error'
           }
-        });
-        return spock.spark.internal.device.register()
-          .then(() => spock.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, true));
-      }));
+        }
+      });
+      return spock.spark.internal.device.register()
+        .then(() => spock.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, true));
+    }));
 
-    before('create mccoy', () => testUsers.create({count: 1, config: {displayName: mccoyName}})
-      .then((users) => {
-        [mccoy] = users;
-      }));
+  before('create mccoy', () => testUsers.create({count: 1, config: {displayName: mccoyName}})
+    .then((users) => {
+      [mccoy] = users;
+    }));
 
-    before('pause to let test users establish', () => browser.pause(5000));
+  before('pause to let test users establish', () => browser.pause(5000));
 
-    before('inject token', () => {
-      if (process.env.DEBUG_JOURNEYS) {
-        console.info('RUN THE FOLLOWING CODE BLOCK TO RERUN THIS TEST FROM DEV TOOLS');
-        console.info();
-        console.info(`window.openSpaceWidget({
+  before('inject token', () => {
+    if (process.env.DEBUG_JOURNEYS) {
+      console.info('RUN THE FOLLOWING CODE BLOCK TO RERUN THIS TEST FROM DEV TOOLS');
+      console.info();
+      console.info(`window.openSpaceWidget({
           accessToken: "${spock.token.access_token}",
           toPersonEmail: "${mccoy.email}",
           initialActivity: "message"
         });`);
-        console.info();
-        console.info();
-      }
-      browserLocal.execute((localAccessToken, localToUserEmail) => {
-        const options = {
-          accessToken: localAccessToken,
-          toPersonEmail: localToUserEmail,
-          initialActivity: 'message'
-        };
-        window.openSpaceWidget(options);
-      }, spock.token.access_token, mccoy.email);
-    });
-
-    if (process.env.DEBUG_JOURNEYS) {
-      console.warn('Running with DEBUG_JOURNEYS may require you to manually kill wdio');
-      // Leaves the browser open for further testing and inspection
-      after(() => browserLocal.debug());
+      console.info();
+      console.info();
     }
+    browserLocal.execute((localAccessToken, localToUserEmail) => {
+      const options = {
+        accessToken: localAccessToken,
+        toPersonEmail: localToUserEmail,
+        initialActivity: 'message'
+      };
+      window.openSpaceWidget(options);
+    }, spock.token.access_token, mccoy.email);
+    browserLocal.waitForVisible(elements.spaceWidget);
+  });
 
+  it('loads the test page', () => {
+    const title = browserLocal.getTitle();
+    assert.equal(title, 'Cisco Spark Widget Test');
+  });
 
-    it('loads the test page', () => {
-      const title = browserLocal.getTitle();
-      assert.equal(title, 'Cisco Spark Widget Test');
+  it('loads the user\'s name', () => {
+    browserLocal.waitForVisible('h1.ciscospark-title');
+    browserLocal.waitUntil(() => browserLocal.getText('h1.ciscospark-title') !== 'Loading...');
+    assert.equal(browserLocal.getText('h1.ciscospark-title'), mccoy.displayName);
+  });
+
+  describe('Activity Menu', () => {
+    it('has a menu button', () => {
+      assert.isTrue(browserLocal.isVisible(elements.menuButton));
     });
 
-    it('loads the user\'s name', () => {
-      browserLocal.waitForVisible('h1.ciscospark-title');
-      browserLocal.waitUntil(() => browserLocal.getText('h1.ciscospark-title') !== 'Loading...');
-      assert.equal(browserLocal.getText('h1.ciscospark-title'), mccoy.displayName);
+    it('displays the menu when clicking the menu button', () => {
+      browserLocal.click(elements.menuButton);
+      browserLocal.waitForVisible(elements.activityMenu);
     });
 
-    describe('Activity Menu', () => {
-      it('has a menu button', () => {
-        assert.isTrue(browserLocal.isVisible(elements.menuButton));
-      });
-
-      it('displays the menu when clicking the menu button', () => {
-        browserLocal.click(elements.menuButton);
-        browserLocal.waitForVisible(elements.activityMenu);
-      });
-
-      it('has a message button', () => {
-        browserLocal.element(elements.controlsContainer).element(elements.messageButton).waitForVisible();
-      });
-
-      it('has a meet button', () => {
-        browserLocal.element(elements.controlsContainer).element(elements.meetButton).waitForVisible();
-      });
-
-      it('has a people button', () => {
-        browserLocal.element(elements.controlsContainer).element(rosterElements.peopleButton).waitForVisible();
-      });
-
-      it('has a files button', () => {
-        browserLocal.element(elements.controlsContainer).element(elements.filesButton).waitForVisible();
-      });
-
-      it('switches to files widget', () => {
-        browserLocal.waitForVisible(elements.filesButton);
-        browserLocal.click(elements.filesButton);
-        browserLocal.waitForVisible(elements.filesWidget);
-        browserLocal.waitForVisible(elements.menuButton);
-        browserLocal.click(elements.menuButton);
-      });
-
-      it('has an exit menu button', () => {
-        assert.isTrue(browserLocal.isVisible(elements.activityMenu));
-        browserLocal.waitForVisible(elements.exitButton);
-      });
-
-      it('closes the menu with the exit button', () => {
-        browserLocal.click(elements.exitButton);
-        browserLocal.waitForVisible(elements.activityMenu, 1500, true);
-      });
-
-      it('has a message button', () => {
-        browserLocal.click(elements.menuButton);
-        browserLocal.element(elements.controlsContainer).element(elements.messageButton).waitForVisible();
-      });
-
-      it('switches to message widget', () => {
-        browserLocal.element(elements.controlsContainer).element(elements.messageButton).click();
-        assert.isTrue(browserLocal.isVisible(elements.messageWidget));
-        assert.isFalse(browserLocal.isVisible(elements.meetWidget));
-      });
-
-      it('has a meet button', () => {
-        browserLocal.click(elements.menuButton);
-        browserLocal.element(elements.controlsContainer).element(elements.meetButton).waitForVisible();
-      });
-
-      it('switches to meet widget', () => {
-        browserLocal.element(elements.controlsContainer).element(elements.meetButton).click();
-        assert.isTrue(browserLocal.isVisible(elements.meetWidget));
-        assert.isFalse(browserLocal.isVisible(elements.messageWidget));
-      });
+    it('has a message button', () => {
+      browserLocal.waitForVisible(elements.messageButton);
     });
 
-    describe('roster tests', () => {
-      before('open roster widget', () => {
-        openMenuAndClickButton(browserLocal, rosterElements.peopleButton);
-        assert.isTrue(browserLocal.isVisible(rosterElements.rosterWidget));
-      });
-
-      it('has a close button', () => {
-        assert.isTrue(
-          browserLocal
-            .element(rosterElements.rosterWidget)
-            .element(rosterElements.closeButton)
-            .isVisible()
-        );
-      });
-
-      it('has the total count of participants', () => {
-        assert.equal(browserLocal.element(rosterElements.rosterTitle).getText(), 'People (2)');
-      });
-
-      it('has the participants listed', () => {
-        hasParticipants(browserLocal, [mccoy, spock]);
-      });
-
-      it('closes the people roster widget', () => {
-        browserLocal.element(rosterElements.rosterWidget).element(rosterElements.closeButton).click();
-        browserLocal.element(rosterElements.rosterWidget).waitForVisible(1500, true);
-      });
+    it('has a meet button', () => {
+      browserLocal.waitForVisible(elements.meetButton);
     });
 
-    describe('accessibility', () => {
-      it('should have no accessibility violations', () =>
-        runAxe(browserLocal, 'ciscospark-widget')
-          .then((results) => {
-            assert.equal(results.violations.length, 0);
-          }));
+    it('has a people button', () => {
+      browserLocal.waitForVisible(rosterElements.peopleButton);
+    });
+
+    it('has a files button', () => {
+      browserLocal.waitForVisible(elements.filesButton);
+    });
+
+    it('switches to files widget', () => {
+      browserLocal.waitForVisible(elements.filesButton);
+      browserLocal.click(elements.filesButton);
+      browserLocal.waitForVisible(elements.filesWidget);
+      browserLocal.waitForVisible(elements.menuButton);
+      browserLocal.click(elements.menuButton);
+    });
+
+    it('has an exit menu button', () => {
+      assert.isTrue(browserLocal.isVisible(elements.activityMenu));
+      browserLocal.waitForVisible(elements.exitButton);
+    });
+
+    it('closes the menu with the exit button', () => {
+      browserLocal.click(elements.exitButton);
+      browserLocal.waitForVisible(elements.activityMenu, 60000, true);
+    });
+
+    it('has a message button', () => {
+      browserLocal.click(elements.menuButton);
+      browserLocal.waitForVisible(elements.messageButton);
+    });
+
+    it('switches to message widget', () => {
+      browserLocal.click(elements.messageButton);
+      assert.isTrue(browserLocal.isVisible(elements.messageWidget));
+      assert.isFalse(browserLocal.isVisible(elements.meetWidget));
+    });
+
+    it('has a meet button', () => {
+      browserLocal.click(elements.menuButton);
+      browserLocal.waitForVisible(elements.meetButton);
+    });
+
+    it('switches to meet widget', () => {
+      browserLocal.click(elements.meetButton);
+      assert.isTrue(browserLocal.isVisible(elements.meetWidget));
+      assert.isFalse(browserLocal.isVisible(elements.messageWidget));
     });
   });
+
+  describe('roster tests', () => {
+    before('open roster widget', () => {
+      openMenuAndClickButton(browserLocal, rosterElements.peopleButton);
+      assert.isTrue(browserLocal.isVisible(rosterElements.rosterWidget));
+    });
+
+    it('has a close button', () => {
+      assert.isTrue(browserLocal.isVisible(rosterElements.closeButton));
+    });
+
+    it('has the total count of participants', () => {
+      assert.equal(browserLocal.getText(rosterElements.rosterTitle), 'People (2)');
+    });
+
+    it('has the participants listed', () => {
+      hasParticipants(browserLocal, [mccoy, spock]);
+    });
+
+    it('closes the people roster widget', () => {
+      browserLocal.click(rosterElements.closeButton);
+      browserLocal.waitForVisible(rosterElements.rosterWidget, 60000, true);
+    });
+  });
+
+  describe('accessibility', () => {
+    it('should have no accessibility violations', () =>
+      runAxe(browserLocal, 'ciscospark-widget')
+        .then((results) => {
+          assert.equal(results.violations.length, 0);
+        }));
+  });
+
+  if (process.env.DEBUG_JOURNEYS) {
+    console.warn('Running with DEBUG_JOURNEYS may require you to manually kill wdio');
+    // Leaves the browser open for further testing and inspection
+    after(() => browserLocal.debug());
+  }
 });
