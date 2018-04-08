7d2750aa1b5c6a727650dd65bdaee661cf3b5836
moriahmaney <moriahmaney@gmail.com>
2018-04-07 20:49:29 -0400

    refactor(journeys): use consistent style across data api one on one tests

--- test/journeys/specs/oneOnOne/dataApi/features.js
+++ test/journeys/specs/oneOnOne/dataApi/features.js
@@ -6,102 +6,95 @@ import testUsers from '@ciscospark/test-helper-test-users';
 
 import {elements as rosterElements, FEATURE_FLAG_ROSTER} from '../../../lib/test-helpers/space-widget/roster';
 
-describe('Widget Space: One on One', () => {
-  describe('Data API', () => {
-    const browserLocal = browser.select('browserLocal');
-    const browserRemote = browser.select('browserRemote');
+describe('Widget Space: One on One: Data API', () => {
+  const browserLocal = browser.select('browserLocal');
+  const browserRemote = browser.select('browserRemote');
 
-    const menuButton = 'button[aria-label="Main Menu"]';
-    const activityMenu = '.ciscospark-activity-menu';
-    const controlsContainer = '.ciscospark-controls-container';
+  const menuButton = 'button[aria-label="Main Menu"]';
+  const activityMenu = '.ciscospark-activity-menu';
 
-    let userWithAllTheFeatures, userWithNoFeatures;
+  let userWithAllTheFeatures, userWithNoFeatures;
 
-    before('load browsers', () => {
-      browser
-        .url('data-api/space.html')
-        .execute(() => {
-          localStorage.clear();
-        });
-    });
+  before('load browsers', () => {
+    browser.url('data-api/space.html');
+  });
 
-    before('create main user', () => testUsers.create({count: 1, config: {displayName: 'All Features'}})
-      .then((users) => {
-        [userWithAllTheFeatures] = users;
-        userWithAllTheFeatures.spark = new CiscoSpark({
-          credentials: {
-            authorization: userWithAllTheFeatures.token
-          },
-          config: {
-            logger: {
-              level: 'error'
-            }
+  before('create main user', () => testUsers.create({count: 1, config: {displayName: 'All Features'}})
+    .then((users) => {
+      [userWithAllTheFeatures] = users;
+      userWithAllTheFeatures.spark = new CiscoSpark({
+        credentials: {
+          authorization: userWithAllTheFeatures.token
+        },
+        config: {
+          logger: {
+            level: 'error'
           }
-        });
-        return userWithAllTheFeatures.spark.internal.device.register()
-          .then(() => userWithAllTheFeatures.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, true));
-      }));
+        }
+      });
+      return userWithAllTheFeatures.spark.internal.device.register()
+        .then(() => userWithAllTheFeatures.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, true));
+    }));
 
-    before('create basic user', () => testUsers.create({count: 1, config: {displayName: 'No Features'}})
-      .then((users) => {
-        [userWithNoFeatures] = users;
-        userWithNoFeatures.spark = new CiscoSpark({
-          credentials: {
-            authorization: userWithNoFeatures.token
-          },
-          config: {
-            logger: {
-              level: 'error'
-            }
+  before('create basic user', () => testUsers.create({count: 1, config: {displayName: 'No Features'}})
+    .then((users) => {
+      [userWithNoFeatures] = users;
+      userWithNoFeatures.spark = new CiscoSpark({
+        credentials: {
+          authorization: userWithNoFeatures.token
+        },
+        config: {
+          logger: {
+            level: 'error'
           }
-        });
-        return userWithNoFeatures.spark.internal.device.register()
-          .then(() => userWithNoFeatures.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, false));
-      }));
+        }
+      });
+      return userWithNoFeatures.spark.internal.device.register()
+        .then(() => userWithNoFeatures.spark.internal.feature.setFeature('developer', FEATURE_FLAG_ROSTER, false));
+    }));
 
-    before('pause to let test users establish', () => browser.pause(5000));
+  before('pause to let test users establish', () => browser.pause(5000));
 
-    before('open widget local', () => {
-      browserLocal.execute((localAccessToken, localToUserEmail) => {
-        const csmmDom = document.createElement('div');
-        csmmDom.setAttribute('class', 'ciscospark-widget');
-        csmmDom.setAttribute('data-toggle', 'ciscospark-space');
-        csmmDom.setAttribute('data-access-token', localAccessToken);
-        csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'message');
-        document.getElementById('ciscospark-widget').appendChild(csmmDom);
-        window.loadBundle('/dist-space/bundle.js');
-      }, userWithAllTheFeatures.token.access_token, userWithNoFeatures.email);
-      browserLocal.waitForVisible(`[placeholder="Send a message to ${userWithNoFeatures.displayName}"]`, 30000);
-    });
+  before('open widget local', () => {
+    browserLocal.execute((localAccessToken, localToUserEmail) => {
+      const csmmDom = document.createElement('div');
+      csmmDom.setAttribute('class', 'ciscospark-widget');
+      csmmDom.setAttribute('data-toggle', 'ciscospark-space');
+      csmmDom.setAttribute('data-access-token', localAccessToken);
+      csmmDom.setAttribute('data-to-person-email', localToUserEmail);
+      csmmDom.setAttribute('data-initial-activity', 'message');
+      document.getElementById('ciscospark-widget').appendChild(csmmDom);
+      window.loadBundle('/dist-space/bundle.js');
+    }, userWithAllTheFeatures.token.access_token, userWithNoFeatures.email);
+    browserLocal.waitForVisible(`[placeholder="Send a message to ${userWithNoFeatures.displayName}"]`);
+  });
 
-    before('open widget remote', () => {
-      browserRemote.execute((localAccessToken, localToUserEmail) => {
-        const csmmDom = document.createElement('div');
-        csmmDom.setAttribute('class', 'ciscospark-widget');
-        csmmDom.setAttribute('data-toggle', 'ciscospark-space');
-        csmmDom.setAttribute('data-access-token', localAccessToken);
-        csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'message');
-        document.getElementById('ciscospark-widget').appendChild(csmmDom);
-        window.loadBundle('/dist-space/bundle.js');
-      }, userWithNoFeatures.token.access_token, userWithAllTheFeatures.email);
-      browserRemote.waitForVisible(`[placeholder="Send a message to ${userWithAllTheFeatures.displayName}"]`, 30000);
-    });
+  before('open widget remote', () => {
+    browserRemote.execute((localAccessToken, localToUserEmail) => {
+      const csmmDom = document.createElement('div');
+      csmmDom.setAttribute('class', 'ciscospark-widget');
+      csmmDom.setAttribute('data-toggle', 'ciscospark-space');
+      csmmDom.setAttribute('data-access-token', localAccessToken);
+      csmmDom.setAttribute('data-to-person-email', localToUserEmail);
+      csmmDom.setAttribute('data-initial-activity', 'message');
+      document.getElementById('ciscospark-widget').appendChild(csmmDom);
+      window.loadBundle('/dist-space/bundle.js');
+    }, userWithNoFeatures.token.access_token, userWithAllTheFeatures.email);
+    browserRemote.waitForVisible(`[placeholder="Send a message to ${userWithAllTheFeatures.displayName}"]`);
+  });
 
-    describe('Feature Flags', () => {
-      describe('Roster Feature Flag', () => {
-        it('has a roster for user with feature flag', () => {
-          browserLocal.click(menuButton);
-          browserLocal.waitForVisible(activityMenu);
-          assert.isTrue(browserLocal.element(controlsContainer).element(rosterElements.peopleButton).isVisible());
-        });
+  describe('Feature Flags', () => {
+    describe('Roster Feature Flag', () => {
+      it('has a roster for user with feature flag', () => {
+        browserLocal.click(menuButton);
+        browserLocal.waitForVisible(activityMenu);
+        assert.isTrue(browserLocal.isVisible(rosterElements.peopleButton));
+      });
 
-        it('does not have a roster for user without flag', () => {
-          browserRemote.click(menuButton);
-          browserRemote.waitForVisible(activityMenu);
-          assert.isFalse(browserRemote.element(controlsContainer).element(rosterElements.peopleButton).isVisible());
-        });
+      it('does not have a roster for user without flag', () => {
+        browserRemote.click(menuButton);
+        browserRemote.waitForVisible(activityMenu);
+        assert.isFalse(browserRemote.isVisible(rosterElements.peopleButton));
       });
     });
   });
--- test/journeys/specs/oneOnOne/dataApi/meet.js
+++ test/journeys/specs/oneOnOne/dataApi/meet.js
@@ -4,81 +4,75 @@ import '@ciscospark/plugin-phone';
 import {switchToMeet} from '../../../lib/test-helpers/space-widget/main';
 import {elements, hangupBeforeAnswerTest, declineIncomingCallTest, hangupDuringCallTest} from '../../../lib/test-helpers/space-widget/meet';
 
-describe('Widget Space: One on One', () => {
-  describe('Data API', () => {
-    const browserLocal = browser.select('browserLocal');
-    const browserRemote = browser.select('browserRemote');
-    let mccoy, spock;
+describe('Widget Space: One on One: Data API', () => {
+  const browserLocal = browser.select('browserLocal');
+  const browserRemote = browser.select('browserRemote');
+  let mccoy, spock;
 
-    before('load browsers', () => {
-      browser
-        .url('/data-api/space.html')
-        .execute(() => {
-          localStorage.clear();
-        });
+  before('load browsers', () => {
+    browser.url('/data-api/space.html');
+  });
+
+  before('create spock', () => testUsers.create({count: 1, config: {displayName: 'Mr Spock'}})
+    .then((users) => {
+      [spock] = users;
+    }));
+
+  before('create mccoy', () => testUsers.create({count: 1, config: {displayName: 'Bones Mccoy'}})
+    .then((users) => {
+      [mccoy] = users;
+    }));
+
+  before('pause to let test users establish', () => browser.pause(5000));
+
+  before('open local widget spock', () => {
+    browserLocal.execute((localAccessToken, localToUserEmail) => {
+      const csmmDom = document.createElement('div');
+      csmmDom.setAttribute('class', 'ciscospark-widget');
+      csmmDom.setAttribute('data-toggle', 'ciscospark-space');
+      csmmDom.setAttribute('data-access-token', localAccessToken);
+      csmmDom.setAttribute('data-to-person-email', localToUserEmail);
+      csmmDom.setAttribute('data-initial-activity', 'message');
+      document.getElementById('ciscospark-widget').appendChild(csmmDom);
+      window.loadBundle('/dist-space/bundle.js');
+    }, spock.token.access_token, mccoy.email);
+    browserLocal.waitForVisible(`[placeholder="Send a message to ${mccoy.displayName}"]`);
+  });
+
+  before('open remote widget mccoy', () => {
+    browserRemote.execute((localAccessToken, localToUserEmail) => {
+      const csmmDom = document.createElement('div');
+      csmmDom.setAttribute('class', 'ciscospark-widget');
+      csmmDom.setAttribute('data-toggle', 'ciscospark-space');
+      csmmDom.setAttribute('data-access-token', localAccessToken);
+      csmmDom.setAttribute('data-to-person-email', localToUserEmail);
+      csmmDom.setAttribute('data-initial-activity', 'message');
+      csmmDom.setAttribute('on-event', 'message');
+      document.getElementById('ciscospark-widget').appendChild(csmmDom);
+      window.loadBundle('/dist-space/bundle.js');
+    }, mccoy.token.access_token, spock.email);
+    browserRemote.waitForVisible(`[placeholder="Send a message to ${spock.displayName}"]`);
+  });
+
+  describe('meet widget', () => {
+    describe('pre call experience', () => {
+      it('has a call button', () => {
+        switchToMeet(browserLocal);
+        browserLocal.waitForVisible(elements.callButton);
+      });
     });
 
-    before('create spock', () => testUsers.create({count: 1, config: {displayName: 'Mr Spock'}})
-      .then((users) => {
-        [spock] = users;
-      }));
-
-    before('create mccoy', () => testUsers.create({count: 1, config: {displayName: 'Bones Mccoy'}})
-      .then((users) => {
-        [mccoy] = users;
-      }));
-
-    before('pause to let test users establish', () => browser.pause(5000));
-
-    before('open local widget spock', () => {
-      browserLocal.execute((localAccessToken, localToUserEmail) => {
-        const csmmDom = document.createElement('div');
-        csmmDom.setAttribute('class', 'ciscospark-widget');
-        csmmDom.setAttribute('data-toggle', 'ciscospark-space');
-        csmmDom.setAttribute('data-access-token', localAccessToken);
-        csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'message');
-        document.getElementById('ciscospark-widget').appendChild(csmmDom);
-        window.loadBundle('/dist-space/bundle.js');
-      }, spock.token.access_token, mccoy.email);
-      browserLocal.waitForVisible(`[placeholder="Send a message to ${mccoy.displayName}"]`, 30000);
-    });
-
-    before('open remote widget mccoy', () => {
-      browserRemote.execute((localAccessToken, localToUserEmail) => {
-        const csmmDom = document.createElement('div');
-        csmmDom.setAttribute('class', 'ciscospark-widget');
-        csmmDom.setAttribute('data-toggle', 'ciscospark-space');
-        csmmDom.setAttribute('data-access-token', localAccessToken);
-        csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'message');
-        csmmDom.setAttribute('on-event', 'message');
-        document.getElementById('ciscospark-widget').appendChild(csmmDom);
-        window.loadBundle('/dist-space/bundle.js');
-      }, mccoy.token.access_token, spock.email);
-      browserRemote.waitForVisible(`[placeholder="Send a message to ${spock.displayName}"]`, 30000);
-    });
-
-    describe('meet widget', () => {
-      describe('pre call experience', () => {
-        it('has a call button', () => {
-          switchToMeet(browserLocal);
-          browserLocal.element(elements.meetWidget).element(elements.callButton).waitForVisible();
-        });
+    describe('during call experience', () => {
+      it('can hangup before answer', () => {
+        hangupBeforeAnswerTest(browserLocal, browserRemote);
       });
 
-      describe('during call experience', () => {
-        it('can hangup before answer', () => {
-          hangupBeforeAnswerTest(browserLocal, browserRemote);
-        });
+      it('can decline an incoming call', () => {
+        declineIncomingCallTest(browserLocal, browserRemote);
+      });
 
-        it('can decline an incoming call', () => {
-          declineIncomingCallTest(browserLocal, browserRemote);
-        });
-
-        it('can hangup in call', () => {
-          hangupDuringCallTest(browserLocal, browserRemote);
-        });
+      it('can hangup in call', () => {
+        hangupDuringCallTest(browserLocal, browserRemote);
       });
     });
   });
