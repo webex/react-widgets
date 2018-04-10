3852bcba765a4357dbd44541cead4ea07e42527e
moriahmaney <moriahmaney@gmail.com>
2018-04-10 10:58:07 -0400

    fix(journeys): create conversation before start call test and add wait

--- test/journeys/specs/oneOnOne/dataApi/startup-settings.js
+++ test/journeys/specs/oneOnOne/dataApi/startup-settings.js
@@ -1,4 +1,6 @@
 import testUsers from '@ciscospark/test-helper-test-users';
+import CiscoSpark from '@ciscospark/spark-core';
+import '@ciscospark/internal-plugin-conversation';
 
 import {moveMouse} from '../../../lib/test-helpers';
 import {elements} from '../../../lib/test-helpers/space-widget/main.js';
@@ -7,7 +9,7 @@ import {answer, hangup, elements as meetElements} from '../../../lib/test-helper
 describe('Widget Space: One on One: Data API Settings', () => {
   const browserLocal = browser.select('browserLocal');
   const browserRemote = browser.select('browserRemote');
-  let mccoy, spock;
+  let mccoy, spock, conversation;
 
   before('load browsers', () => {
     browser.url('/data-api/space.html');
@@ -16,14 +18,44 @@ describe('Widget Space: One on One: Data API Settings', () => {
   before('create spock', () => testUsers.create({count: 1, config: {displayName: 'Mr Spock'}})
     .then((users) => {
       [spock] = users;
+      spock.spark = new CiscoSpark({
+        credentials: {
+          authorization: spock.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+      return spock.spark.internal.mercury.connect();
     }));
 
   before('create mccoy', () => testUsers.create({count: 1, config: {displayName: 'Bones Mccoy'}})
     .then((users) => {
       [mccoy] = users;
+      mccoy.spark = new CiscoSpark({
+        credentials: {
+          authorization: mccoy.token
+        },
+        config: {
+          logger: {
+            level: 'error'
+          }
+        }
+      });
+      return mccoy.spark.internal.mercury.connect();
     }));
 
   before('pause to let test users establish', () => browser.pause(5000));
+
+
+  before('create space', () => mccoy.spark.internal.conversation.create({
+    participants: [mccoy, spock]
+  }).then((c) => {
+    conversation = c;
+    return conversation;
+  }));
 
   describe('initial activity setting: meet', () => {
     before('inject token', () => {
@@ -62,6 +94,7 @@ describe('Widget Space: One on One: Data API Settings', () => {
 
     it('opens message widget', () => {
       browserLocal.waitForVisible(elements.messageWidget);
+      browserLocal.waitForVisible(`[placeholder="Send a message to ${mccoy.displayName}"]`);
       browserLocal.refresh();
     });
   });
@@ -74,11 +107,12 @@ describe('Widget Space: One on One: Data API Settings', () => {
         csmmDom.setAttribute('data-toggle', 'ciscospark-space');
         csmmDom.setAttribute('data-access-token', localAccessToken);
         csmmDom.setAttribute('data-to-person-email', localToUserEmail);
-        csmmDom.setAttribute('data-initial-activity', 'meet');
+        csmmDom.setAttribute('data-initial-activity', 'message');
         document.getElementById('ciscospark-widget').appendChild(csmmDom);
         window.loadBundle('/dist-space/bundle.js');
       }, mccoy.token.access_token, spock.email);
-      browserRemote.waitForVisible(elements.meetWidget);
+      browserRemote.waitForVisible(elements.messageWidget);
+      browserRemote.waitForVisible(`[placeholder="Send a message to ${spock.displayName}"]`);
     });
 
     before('inject token', () => {
@@ -97,6 +131,7 @@ describe('Widget Space: One on One: Data API Settings', () => {
     });
 
     it('starts call when set to true', () => {
+      browser.pause(5000);
       answer(browserRemote);
       moveMouse(browserLocal, meetElements.callContainer);
       hangup(browserLocal);
