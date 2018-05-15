56fb11be0810a6ce094a0feb6e7ad2d5ca1fe808
Adam Weeks <adweeks@cisco.com>
2018-05-15 11:26:15 -0400

    refactor: remove icon types from main export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 export {default as Button} from '@ciscospark/react-component-button';
-export {
-  default as Icon,
-  ICON_TYPE_ADD,
-  ICON_TYPE_DELETE,
-  ICON_TYPE_DOCUMENT,
-  ICON_TYPE_DOWNLOAD,
-  ICON_TYPE_FLAGGED,
-  ICON_TYPE_FLAGGED_OUTLINE,
-  ICON_TYPE_MESSAGE,
-  ICON_TYPE_RIGHT_ARROW
-} from '@ciscospark/react-component-icon';
+export {default as Icon} from '@ciscospark/react-component-icon';
 export {default as AddFileButton} from '@ciscospark/react-component-add-file-button';
 export {default as Avatar} from '@ciscospark/react-component-avatar';
 export {default as ChipBase} from '@ciscospark/react-component-chip-base';
