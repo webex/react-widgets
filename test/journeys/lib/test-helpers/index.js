8ae254e41608e93b9444cad544c455572d55e722
Adam Weeks <adweeks@cisco.com>
2018-10-09 17:20:23 -0400

    chore(journeys): convert sauce helpers to promise based

--- test/journeys/lib/test-helpers/index.js
+++ test/journeys/lib/test-helpers/index.js
@@ -11,9 +11,10 @@ export const jobNames = {
   spaceGuest: 'react-widget-space-guest',
   spaceDataApi: 'react-widget-space-dataApi',
   spaceStartup: 'react-widget-space-startup',
-  smokeMultiple: 'react-widget-multiple-smoke',
-  smokeRecents: 'react-widget-recents-smoke',
-  smokeSpace: 'react-widget-space-smoke'
+  smokeInitial: 'react-widget-smoke-initial',
+  smokeMultiple: 'react-widget-smoke-multiple',
+  smokeRecents: 'react-widget-smoke-recents',
+  smokeSpace: 'react-widget-smoke-space'
 };
 /**
  * Move mouse a specified amount of pixels
@@ -52,63 +53,90 @@ export function moveMouse(aBrowser, selector) {
 }
 
 /**
- * Reload each active sauce session to a fresh session
- * and name session according to suite
+ * Gets the widget jobs that includes the name passed
  * @param {string} name
- * @param {object} browser
- * @returns {void}
+ * @returns {Promise}
  */
-export function renameJob(name, browser) {
-  if (!process.env.SAUCE) {
-    return;
-  }
-  browser.reload();
-  const browserName = process.env.BROWSER || 'chrome';
-  const platform = process.env.PLATFORM || 'mac 10.12';
-  const {suite} = argv || 'smoke';
+function getWidgetJobs(name) {
+  return new Promise((resolve, reject) => {
+    const build = process.env.BUILD_NUMBER;
+    const account = new SauceLabs({
+      username: process.env.SAUCE_USERNAME,
+      password: process.env.SAUCE_ACCESS_KEY
+    });
+    const browserName = process.env.BROWSER || 'chrome';
+    const platform = process.env.PLATFORM || 'mac 10.12';
 
-  const account = new SauceLabs({
-    username: process.env.SAUCE_USERNAME,
-    password: process.env.SAUCE_ACCESS_KEY
-  });
-  account.getJobs((err, jobs) => {
-    // Something terrible has happened if we cannot get jobs from sauce
-    if (err) {
-      throw err;
-    }
-    // Job name `react-widget-${suite}` comes from wdio.conf.js
-    const widgetJobs = jobs.filter((job) => job.name && job.name.includes(`react-widget-${suite}`) && job.status === 'in progress'
-          && job.os.toLowerCase().includes(platform) && job.browser.toLowerCase().includes(browserName));
-    widgetJobs.forEach((job) => {
-      const remoteName = job.name.includes('local') ? 'local' : 'remote';
-      const jobName = `${name}-${remoteName}`;
-      // Only update name if it needs it
-      if (job.name !== jobName) {
-        account.updateJob(job.id, {name: jobName});
+    account.getJobs((err, jobs) => {
+      // Something terrible has happened if we cannot get jobs from sauce
+      if (err) {
+        reject(err);
       }
+      const widgetJobs = jobs.filter((job) => job.build === build && job.name && job.name.includes(name) && job.status === 'in progress'
+              && job.os.toLowerCase().includes(platform) && job.browser.toLowerCase().includes(browserName));
+      resolve(widgetJobs);
     });
   });
 }
 
+/**
+ * Updates the job on sauce
+ * @param {string} id
+ * @param {object} details
+ * @returns {Promise}
+ */
+function updateJob(id, details) {
+  return new Promise((resolve, reject) => {
+    const account = new SauceLabs({
+      username: process.env.SAUCE_USERNAME,
+      password: process.env.SAUCE_ACCESS_KEY
+    });
+    account.updateJob(id, details, (err, response) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(response);
+    });
+  });
+}
+
+/**
+ * Reload each active sauce session to a fresh session
+ * and name session according to suite
+ * @param {string} name
+ * @param {object} browser
+ * @returns {Promise}
+ */
+export function renameJob(name) {
+  if (!process.env.SAUCE) {
+    return Promise.resolve();
+  }
+
+  const {suite} = argv || 'smoke';
+  // 'unnamed' jobs are those which haven't been renamed
+  // When we do browser.reload, it will create a new job with the initial name
+  return getWidgetJobs(`react-widget-${suite}-unnamed`).then((widgetJobs) => {
+    const promises = widgetJobs.map((job) => {
+      const remoteName = job.name.includes('local') ? 'local' : 'remote';
+      const jobName = `${name}-${remoteName}`;
+      return updateJob(job.id, {name: jobName});
+    });
+    return Promise.all(promises);
+  });
+}
+
+/**
+ * Updates a job's status
+ * @param {string} name
+ * @param {boolean} passed
+ * @returns {Promise}
+ */
 export function updateJobStatus(name, passed) {
   if (!process.env.SAUCE) {
-    return;
+    return Promise.resolve();
   }
-  const browserName = process.env.BROWSER || 'chrome';
-  const platform = process.env.PLATFORM || 'mac 10.12';
-
-  const account = new SauceLabs({
-    username: process.env.SAUCE_USERNAME,
-    password: process.env.SAUCE_ACCESS_KEY
-  });
-  account.getJobs((err, jobs) => {
-    // Something terrible has happened if we cannot get jobs from sauce
-    if (err) {
-      throw err;
-    }
-
-    const widgetJobs = jobs.filter((job) => job.name === name && job.status === 'in progress'
-            && job.os.toLowerCase().includes(platform) && job.browser.toLowerCase().includes(browserName));
-    widgetJobs.forEach((job) => account.updateJob(job.id, {passed}));
+  return getWidgetJobs(name).then((widgetJobs) => {
+    const promises = widgetJobs.map((job) => updateJob(job.id, {passed}));
+    return Promise.all(promises);
   });
 }
