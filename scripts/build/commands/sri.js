b737666e0fe0b04d79e0d6b923cf84749068e54e
Adam Weeks <adweeks@cisco.com>
2019-10-30 15:19:27 -0400

    chore(sri): support string for sri signature

--- scripts/build/commands/sri.js
+++ scripts/build/commands/sri.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const {generateDistSRI} = require('../../utils/sri');
 
 module.exports = {
@@ -5,18 +7,28 @@ module.exports = {
   desc: 'Generates SRI hash for all files and files in the sub-directories of /dist for the package',
   builder: {},
   handler: ({packageName}) => {
-    if (!process.env.PRIVATE_KEY_PATH) {
-      console.error('No private key found! Please set your PRIVATE_KEY_PATH environment variable');
+    if (!process.env.PRIVATE_KEY) {
+      console.error('No private key found! Please set your PRIVATE_KEY environment variable');
 
       return false;
     }
 
+    let privateKeyString = process.env.PRIVATE_KEY;
+
+    // Since ENV Vars might not support newline characters, let's add them back
+    // by replacing the string "\n" with actual new lines.
+    privateKeyString = privateKeyString.replace(/\\n/g, '\n');
+
+    // Crypto requires private key to be a buffer
+    const privateKey = Buffer.from(privateKeyString, 'utf8');
+
+    const publicKey = fs.readFileSync('./widget-key.pub', 'utf8');
+
     if (packageName) {
       return generateDistSRI({
         packagePath: `./packages/node_modules/@ciscospark/${packageName}`,
-        privateKeyPath: process.env.PRIVATE_KEY_PATH,
-        publicKeyPath: './public-key.pem',
-        passphrase: process.env.PRIVATE_KEY_PASSPHRASE
+        privateKey,
+        publicKey
       });
     }
 
