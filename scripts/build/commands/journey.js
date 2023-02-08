1e7d1b2e57fc5e520446fab8750b0a7baef5a743
nvanganm <nvanganm@cisco.com>
2023-02-08 11:01:11 +0530

    added webex call history

--- scripts/build/commands/journey.js
+++ scripts/build/commands/journey.js
@@ -33,7 +33,8 @@ module.exports = {
           execSync(`BUILD_DIST_PATH=${dest}/dist-space npm run build:package widget-space`);
           execSync(`BUILD_DIST_PATH=${dest}/dist-recents npm run build:package widget-recents`);
           execSync(`BUILD_DIST_PATH=${dest}/dist-demo npm run build:package widget-demo`);
-          execSync(`BUILD_DIST_PATH=${dest}/dist-demo npm run build:package widget-calling`);
+          // execSync(`BUILD_DIST_PATH=${dest}/dist-demo npm run build:package widget-calling`);
+          execSync(`BUILD_DIST_PATH=${dest}/dist-demo npm run build:package widget-call-history`);
         })
         .catch((err) => {
           console.log(err);
